d7cc83d08302dba61f525fb1555afa05fe8599d2
Luiz-021 <luizgute@outlook.com>
2025-03-22 18:31:31 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Departament obj = new Departament(1, "Books");
+        System.out.println(obj);
+    }
+}
