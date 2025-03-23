e4d7b79c61890ee6ce49dbf2e5b7aa357d7ff37b
Luiz-021 <luizgute@outlook.com>
2025-03-23 16:52:22 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -14,76 +14,86 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
-    private Connection conn;
+  private Connection conn;
 
-    public SellerDaoJDBC(Connection conn) {
-        this.conn = conn;
-    }
+  public SellerDaoJDBC(Connection conn) {
+    this.conn = conn;
+  }
 
-    @Override
-    public void insert(Seller obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
-    }
+  @Override
+  public void insert(Seller obj) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'insert'");
+  }
 
-    @Override
-    public void update(Seller obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
-    }
+  @Override
+  public void update(Seller obj) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  }
 
-    @Override
-    public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
-    }
+  @Override
+  public void deleteById(Integer id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+  }
 
-    @Override
-    public Seller findById(Integer id) {
-       PreparedStatement st = null;
-       ResultSet rs = null;
-       try{
-              st = conn.prepareStatement(  
-                "SELECT seller.*,department.Name as DepName "
-                + "FROM seller inner join department " 
-                + "ON seller.DepartmentId = department.Id "
-                + "WHERE seller.Id = ?");
-                
-              st.setInt(1, id);
-              rs = st.executeQuery();
-              if(rs.next()){
-                Departament dep = new Departament();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartament(dep);
-                return obj;
-              }
-            return null;
-       }
-         catch(SQLException e){
-              throw new DbException(e.getMessage());
-         }
-         finally{
-              try{
-                DB.closeStatement(st);
-                DB.closeResultSet(rs);
-              }
-              catch(Exception e){
-                throw new RuntimeException(e);
-              }
-         }
-    }
+  @Override
+  public Seller findById(Integer id) {
+      PreparedStatement st = null;
+      ResultSet rs = null;
+      try{
+            st = conn.prepareStatement(  
+              "SELECT seller.*,department.Name as DepName "
+              + "FROM seller inner join department " 
+              + "ON seller.DepartmentId = department.Id "
+              + "WHERE seller.Id = ?");
+              
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()){
+              Departament dep = instantiateDepartament(rs);
+              Seller obj = instatiateSeller(rs, dep);
+              return obj;
+            }
+          return null;
+      }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            try{
+              DB.closeStatement(st);
+              DB.closeResultSet(rs);
+            }
+            catch(Exception e){
+              throw new RuntimeException(e);
+            }
+        }
+  }
 
-    @Override
-    public List<Seller> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
-    }
+  private Departament instantiateDepartament(ResultSet rs) throws SQLException {
+    Departament dep = new Departament();
+    dep.setId(rs.getInt("DepartmentId"));
+    dep.setName(rs.getString("DepName"));
+    return dep;
+  }
+
+  private Seller instatiateSeller(ResultSet rs, Departament dep) throws SQLException {
+    Seller obj = new Seller();
+    obj.setId(rs.getInt("Id"));
+    obj.setName(rs.getString("Name"));
+    obj.setEmail(rs.getString("Email"));
+    obj.setBaseSalary(rs.getDouble("BaseSalary"));
+    obj.setBirthDate(rs.getDate("BirthDate"));
+    obj.setDepartament(dep);
+    return obj;
+  }
+
+  @Override
+  public List<Seller> findAll() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+  }
     
 }
